c382bb1accb13bd724a78124f2ca8b3b40722509
kathrinemelgithub <kathrinemellabandilo@gmail.com>
2024-05-18 15:13:11 -0930

    Initial Commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,23 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String[] question ={
+            "Who wrote the play Romeo and Juliet?",
+            "Which planet is known as the Red Planet?",
+            "What is the capital of Canada?"
+    };
+
+    public static String[][] choices = {
+            {"Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"},
+            {"Saturn", "Venus", "Jupiter", "Mars"},
+            {"Montreal", "Ottawa", "Vancouver", "Toronto"}
+    };
+
+
+    public static String[] correctAnswer ={
+            "William Shakespeare",
+            "Mars",
+            "Ottawa"
+    };
+}
